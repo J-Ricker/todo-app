4c11caedeaf9d0f78c38cec03454dafb8b2e8b4e
Jeremy R <lvlgrinddesign@gmail.com>
2019-02-28 06:50:39 -0500

    used for each and remove to remove items with get

--- todo.js
+++ todo.js
@@ -1,40 +1,7 @@
-const todos = [{
-    text: 'order cat food',
-    completed: true
-}, {
-    text: 'order dog food',
-    completed: false
-}, {
-    text: 'buy food for me',
-    completed: true
-}, {
-    text: 'do work',
-    completed: false
-}, {
-    text: 'exercise',
-    completed: true
-}];
+const p = document.querySelectorAll('p');
 
-// const removeTodo = function (todos, todoText) {
-//     const index = todos.findIndex(function (todo) {
-//         return todo.text.toLowerCase() === todoText.toLowerCase()
-//     })
-//     if (index > -1) {
-//         todos.splice(index, 1);
-//     } else {
-//         console.log('no todo matches')
-//     }
-// }
-
-const getThingsTodo = function(todos) {
-    return todos.filter(function (todo, index) {
-        return !todo.completed;
-    })
-}
-
-
-console.log(getThingsTodo(todos));
-
-
-// removeTodo(todos, 'order Cat food!');
-// console.log(todos);
+p.forEach(function(para) {
+    if (para.textContent.toLowerCase().includes('get')) {
+        para.remove();
+    }
+})
